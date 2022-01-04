ef977b91fba79b5a228d9061f7c864bcfd31aaee
dawidbros1 <dawidbros1@wp.pl>
2022-01-04 14:36:04 +0100

    poprawkka

--- templates/component/form/button.php
+++ templates/component/form/button.php
@@ -1,4 +1,4 @@
-<div class = "text-center <?=$params['div'] ?? ""?>">
+<div class = "text-center mt-2 <?=$params['div'] ?? ""?>">
     <button class="btn btn-primary w-100 <?=$params['button'] ?? ""?>" type="submit">
         <?=$params['text']?>
     </button>
