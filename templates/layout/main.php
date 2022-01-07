14dd9b10959b2cf979d5b90e6aa2e57d2035dde6
dawidbros1 <dawidbros1@wp.pl>
2022-01-07 10:01:36 +0100

    dodanie tagu viewport

--- templates/layout/main.php
+++ templates/layout/main.php
@@ -3,6 +3,7 @@
 <head>
     <title><?=$title?></title>
     <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
