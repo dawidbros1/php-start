1135fbb96e530fe5427ec56179017d071c468e25
dawidbros1 <dawidbros1@wp.pl>
2022-05-23 12:51:36 +0200

    change in methods names and params names | change returned value in getRule() method

--- src/Model/Rules.php
+++ src/Model/Rules.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Model;
 
@@ -18,58 +18,61 @@ abstract class Rules
     }
 
     // Podstawowe metody do tworzenia reguł oraz wiadomości
-    public function createRules(string $name, array $data)
+    public function createRules(string $type, array $rules)
     {
-        foreach ($data as $key => $value) {
-            $this->rules[$name][$key]['value'] = $value;
+        foreach ($rules as $name => $value) {
+            $this->rules[$type][$name]['value'] = $value;
         }
     }
 
-    public function createMessages(string $name, array $data)
+    public function createMessages(string $type, array $rules)
     {
-        foreach ($data as $key => $message) {
-
-            if ($key == "between") {
-                $this->rules[$name]['min']['message'] = $message;
-                $this->rules[$name]['max']['message'] = $message;
+        foreach ($rules as $name => $message) {
+            if ($name == 'between') {
+                $this->rules[$type]['min']['message'] = $message;
+                $this->rules[$type]['max']['message'] = $message;
             } else {
-                $this->rules[$name][$key]['message'] = $message;
+                $this->rules[$type][$name]['message'] = $message;
             }
         }
     }
 
-    // ===== ===== ===== ===== =====
-
+    // ===== ===== USE FROM NameRules like AuthRules ===== =====
     public function value(?string $name = null)
     {
-        return $this->getParam($name, 'value');
+        return $this->getRule($name)['value'];
     }
 
     public function message(?string $name = null)
     {
-        return $this->getParam($name, 'message');
+        return $this->getRule($name)['message'];
     }
 
     public function arrayValue(string $name, bool $uppercase = false)
     {
         $type = strtok($name, '.');
-        $param = substr($name, strpos($name, ".") + 1);
-        $output = "";
+        $rule = substr($name, strpos($name, '.') + 1);
+        $output = '';
 
-        foreach ($this->rules[$type][$param]['value'] as $value) {
-            $output .= ($value . ", ");
+        foreach ($this->rules[$type][$rule]['value'] as $value) {
+            $output .= $value . ', ';
         }
 
-        if ($uppercase) {$output = strtoupper($output);}
+        if ($uppercase) {
+            $output = strtoupper($output);
+        }
         $output = substr($output, 0, -2);
         return $output;
     }
 
     // ===== ===== ===== ===== =====
-
     public function hasType(string $type)
     {
-        if (array_key_exists($type, $this->rules)) {return true;} else {return false;}
+        if (array_key_exists($type, $this->rules)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function selectType(string $type)
@@ -77,7 +80,6 @@ abstract class Rules
         if (!$this->hasType($type)) {
             throw new AppException('Wybrany typ nie istnieje');
         }
-
         $this->selectedType = $type;
     }
 
@@ -86,20 +88,20 @@ abstract class Rules
         $this->selectedType = null;
     }
 
-    public function get(?string $type = null)
+    public function getType(?string $type = null)
     {
-        if ($this->selectedType != null) {
-            return $this->rules[$this->selectedType];
-        } else {
-            if ($type == null) {
+        if ($type === null) {
+            if ($this->selectedType !== null) {
+                return $this->rules[$this->selectedType];
+            } else {
                 throw new AppException('Typ reguły nie został wprowadzony');
             }
-
+        } else {
             if (!$this->hasType($type)) {
                 throw new AppException('Wybrany typ nie istnieje');
+            } else {
+                return $this->rules[$type];
             }
-
-            return $this->rules[$type];
         }
     }
 
@@ -107,9 +109,9 @@ abstract class Rules
     {
         if ($this->selectedType != null) {
             $rules = $this->rules[$this->selectedType];
-        } else if ($type == null) {
+        } elseif ($type == null) {
             throw new AppException('Typ reguły nie został wprowadzony');
-        } else if (!$this->hasType($type)) {
+        } elseif (!$this->hasType($type)) {
             throw new AppException('Wybrany typ nie istnieje');
         } else {
             $rules = $this->rules[$type];
@@ -126,15 +128,14 @@ abstract class Rules
 
     // ===== ===== ===== ===== =====
 
-    private function getParam($name, $param)
+    private function getRule(string $name)
     {
         if ($this->selectedType) {
             if (!array_key_exists($name, $this->rules[$this->selectedType])) {
                 throw new AppException('Wybrana reguła nie istnieje');
             }
 
-            $message = $this->rules[$this->selectedType][$name][$param];
-
+            $message = $this->rules[$this->selectedType][$name];
         } else {
             $type = strtok($name, '.');
 
@@ -142,13 +143,13 @@ abstract class Rules
                 throw new AppException('Wprowadzony typ reguły nie istnieje');
             }
 
-            $rule = substr($name, strpos($name, ".") + 1);
+            $rule = substr($name, strpos($name, '.') + 1);
 
             if (!array_key_exists($rule, $this->rules[$type])) {
                 throw new AppException('Wybrana reguła nie istnieje');
             }
 
-            $message = $this->rules[$type][$rule][$param];
+            $message = $this->rules[$type][$rule];
         }
 
         return $message;
