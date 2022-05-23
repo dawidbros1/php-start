a709632ac3618f0ccb306cafdc58b4682671ecc9
dawidbros1 <dawidbros1@wp.pl>
2022-05-23 12:47:46 +0200

    Added comment

--- src/Rules/AuthRules.php
+++ src/Rules/AuthRules.php
@@ -10,6 +10,7 @@ class AuthRules extends Rules
 {
     public function rules()
     {
+        // (TYPE, [name => value, name => value])
         $this->createRules('username', ['min' => 3, "max" => 16, 'specialCharacters' => true]);
         $this->createRules('password', ['min' => 6, 'max' => 36]);
         $this->createRules('email', ['sanitize' => true, "validate" => true]);
