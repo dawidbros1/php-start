410d28826e622088a48af26c9d8a957d1e614095
dawidbros1 <dawidbros1@wp.pl>
2022-06-15 14:02:30 +0200

    now is using new validation rule: between

--- src/Rules/AuthRules.php
+++ src/Rules/AuthRules.php
@@ -11,20 +11,20 @@ class AuthRules extends Rules
     public function rules()
     {
         // (TYPE, [name => value, name => value])
-        $this->createRule('username', ['min' => 3, "max" => 16, 'specialCharacters' => false]);
-        $this->createRule('password', ['min' => 6, 'max' => 36]);
+        $this->createRule('username', ['between' => ['min' => 3, "max" => 16], 'specialCharacters' => false]);
+        $this->createRule('password', ['between' => ['min' => 6, 'max' => 36]]);
         $this->createRule('email', ['sanitize' => true, "validate" => true]);
     }
 
     public function messages()
     {
         $this->createMessages('username', [
-            'between' => "Nazwa użytkownika powinna zawierać od " . $this->value('username.min') . " do " . $this->value('username.max') . " znaków",
+            'between' => "Nazwa użytkownika powinna zawierać od " . $this->between('username.min') . " do " . $this->between('username.max') . " znaków",
             'specialCharacters' => "Nazwa użytkownika nie może zawierać znaków specjalnych",
         ]);
 
         $this->createMessages('password', [
-            'between' => "Hasło powinno zawierać od " . $this->value('password.min') . " do " . $this->value('password.max') . " znaków",
+            'between' => "Hasło powinno zawierać od " . $this->between('password.min') . " do " . $this->between('password.max') . " znaków",
         ]);
 
         $this->createMessages('email', [
