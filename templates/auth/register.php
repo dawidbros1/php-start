40efbf648497c407293b7adde8c8183afba44e58
dawidbros1 <dawidbros1@wp.pl>
2022-08-02 11:31:21 +0200

    return old view

--- templates/auth/register.php
+++ templates/auth/register.php
@@ -9,24 +9,29 @@ use App\Helper\Session;
 
 <div class="mt-sm-5 pt-sm-5">
     <div class="rounded d-flex justify-content-center">
-         <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 col-12 shadow-lg p-5 bg-light">
+        <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 col-12 shadow-lg p-5 bg-light">
             <div class="text-center">
-                <h3 class="text-primary">Ustaw nowe hasło</h3>
+                <h3 class="text-primary">Rejestracja</h3>
             </div>
             <div class="p-4">
-                <form action="<?=$route->get('auth.resetPassword')?>" method="post">
+                <form action="<?=$route->get('auth.register')?>" method="post">
+                    <?php Component::render('form.input', ['class' => "", 'type' => "email", 'name' => "email", "placeholder" => "Adres email", 'value' => $params['email'] ?? ''])?>
+                    <?php Component::render('error', ['type' => "email", 'names' => ['sanitize', 'validate', 'unique']])?>
 
-                    <?php Component::render('form.input', ['class' => "mt-3", 'value' => $params['email'] ?? "", 'disabled' => 'disabled'])?>
+                    <?php Component::render('form.input', ['class' => "mt-3", 'type' => "text", 'name' => "username", "placeholder" => "Nazwa użytkownika", 'value' => $params['username'] ?? ''])?>
+                    <?php Component::render('error', ['type' => "username", 'names' => ['between', 'specialCharacters']])?>
 
-                    <?php Component::render('form.input', ['class' => "mt-3", 'type' => "password", 'name' => "password", 'description' => "Nowe hasło"])?>
+                    <?php Component::render('form.input', ['class' => "mt-3", 'type' => "password", 'name' => "password", "placeholder" => "Hasło"])?>
                     <?php Component::render('error', ['type' => "password", 'names' => ['between']])?>
 
-                    <?php Component::render('form.input', ['class' => "mt-3", 'type' => "password", 'name' => "repeat_password", 'description' => "Powtórz nowe hasło"])?>
-                    <?php Component::render('error', ['type' => "password", 'names' => ['same']])?>
+                    <?php Component::render('form.input', ['class' => "mt-3", 'type' => "password", 'name' => "repeat_password", "placeholder" => "Powtórz hasło"])?>
+                    <?php Component::render('error', ['type' => "repeat_password", 'names' => ['same']])?>
 
-                    <input type = "hidden" name = "code" value = "<?=$params['code']?>">
+                    <?php Component::render('form.submit', ['text' => "Utwórz konto"])?>
 
-                    <?php Component::render('form.button', ['text' => "Przypomnij hasło"])?>
+                    <p class="text-center mt-3">Masz już konto?
+                        <a href="<?=$route->get('auth.login')?>" class="link">Zaloguj się</a>
+                    </p>
                 </form>
             </div>
         </div>
