4acc44ec675be0ca71aa47a11c7b88b28e132b9e
dawidbros1 <dawidbros1@wp.pl>
2022-09-27 10:53:52 +0200

    change auth to authorization

--- framework/routes/routes.php
+++ framework/routes/routes.php
@@ -6,7 +6,8 @@ use Phantom\Model\Route;
 
 $route = new Route();
 $route->group('', ['home', 'policy', 'contact', 'regulations']);
-$route->group('auth', ['register', 'login', 'forgotPassword', 'resetPassword']);
+$route->group('authorization', ['login', 'forgotPassword', 'resetPassword']);
 $route->group('user', ['logout', 'profile', 'update']);
+$route->register("registration", '');
 
 return $route;
