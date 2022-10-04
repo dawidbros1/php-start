ffcee5f122c986a84fdfe91856b2d375aa1b00e1
dawidbros1 <dawidbros1@wp.pl>
2022-10-04 13:25:42 +0200

    added file routes/basic.php

--- routes/routes.php
+++ routes/routes.php
@@ -2,31 +2,7 @@
 
 declare (strict_types = 1);
 
-use Phantom\Model\Route;
-
-$route = new Route($location);
-
-$route->homepage('home');
-
-$route->group("", "", [
-    'policy' => "/policy",
-    'contact' => "/contact",
-    'regulations' => "/regulations",
-]);
-
-$route->register("registration", "/register");
-$route->register("authorization", "/login");
-
-$route->group('user', "/user", [
-    'logout' => "/logout",
-    'profile' => "/profile",
-    'update' => "/profile/update",
-]);
-
-$route->group('passwordRecovery', "/password", [
-    'forgot' => "/forgot",
-    'reset' => "/reset",
-]);
+$route = require_once "basic.php";
 
 // $route->group('test', "/test", [
 //     'show' => "/show/{id}",
